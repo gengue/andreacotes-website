ceaec076d4a70980c6a80da7474d3bdf41c48863
Genesis Guerrero <genesisdaft@gmail.com>
2026-05-22 18:44:49 +0200

    refactor(interest-list): use shadcn Badge primitives

--- src/components/interest-list.tsx
+++ src/components/interest-list.tsx
@@ -1,14 +1,24 @@
+import { Badge } from "@/components/ui/badge";
+
+const interests = [
+  "Parasitology",
+  "Ticks and Tick-borne Diseases",
+  "Molecular biology",
+  "I love dogs 🐶",
+  "Yoga",
+];
+
 export default function InterestList() {
   return (
     <div className="w-full">
       <h3 className="text-xl">Interests</h3>
-      <ul className="my-2  mx-2 list-disc list-inside">
-        <li>Parasitology</li>
-        <li>Ticks and Tick-borne Diseases</li>
-        <li>Molecular biology</li>
-        <li>I love dogs 🐶</li>
-        <li>Yoga</li>
-      </ul>
+      <div className="mt-3 flex flex-wrap gap-2">
+        {interests.map((interest) => (
+          <Badge key={interest} variant="secondary" className="text-sm">
+            {interest}
+          </Badge>
+        ))}
+      </div>
     </div>
   );
 }
